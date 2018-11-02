0e15c1f1913803342361f728ae8a1b1991ba5e42
Ed McAdams <teddmcadams@outlook.com>
2018-11-01 23:32:00 -0400

    refactor info class

--- StudentMajors/StudentInformation.cs
+++ StudentMajors/StudentInformation.cs
@@ -1,11 +1,10 @@
 using System;
-using System.IO;
-    
+
 namespace StudentMajors
 {
     public class StudentInformation
     {
-        public long Id { get; set; }
+        public long StudentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string AcademicMajor { get; set; }
