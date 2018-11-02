36641df9a48566f5d9111b21f1a0f1120811385f
Ed McAdams <teddmcadams@outlook.com>
2018-11-01 23:02:21 -0400

    refactor student info class

--- StudentMajors/Info.cs
+++ StudentMajors/Info.cs
@@ -1,55 +1,13 @@
 using System;
 using System.IO;
     
-
-namespace student_majors
+namespace StudentMajors
 {
-
-    public class info
+    public class StudentInformation
     {
-        private string first;
-        private string last;
-        private long id;
-        private string major;
-        
-        public string getFirst()
-        {
-            return (first);
-        }
-
-        public void setFirst(string f)
-        {
-            first = f;
-        }
-
-        public string getLast()
-        {
-            return (last);
-        }
-
-        public void setLast(string x)
-        {
-            last = x;
-        }
-
-        public long getID()
-        {
-            return (id);
-        }
-
-        public void setID(long i)
-        {
-            id = i;
-        }
-
-        public string getMajor()
-        {
-            return (major);
-        }
-
-        public void setMajor(string m)
-        {
-            major = m;
-        }
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AcademicMajor { get; set; }
     }
 }
